6f16d0d23a7a9818657548a3878de22e9c329f4d
Pavlik1996 <miki2012miki@gmail.com>
2022-10-17 17:02:20 +0300

    five

--- main.js
+++ main.js
@@ -17,12 +17,6 @@ function calc(action, a, b) {
   }
 }
 
-
 console.log(calc(Subtract, 3, 2));
 console.log(calc(Multi, 1, 2));
 console.log(calc(Add, 1, 2));
-console.log(calc(Add, 1, 2));
-console.log(calc(Add, 1, 2));
-
-
-
