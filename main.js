f84c71b0922051c6f3bce7879b0377d944498761
Pavlik1996 <miki2012miki@gmail.com>
2022-10-17 13:09:12 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const Add = "add";
+const Multi = "multi";
+const Subtract = "subtract";
+
+function calc(action, a, b) {
+  if (Number.isInteger(a, b)) {
+    switch (action) {
+      case Add:
+        return a + b;
+      case Multi:
+        return a * b;
+      case Subtract:
+        return a - b;
+    }
+  } else {
+    return console.log("Не число");
+  }
+}
+
+alert(calc(Subtract, 3, 2));
+console.log(calc(Multi, 1, 2));
+console.log(calc(Add, 1, 2));
